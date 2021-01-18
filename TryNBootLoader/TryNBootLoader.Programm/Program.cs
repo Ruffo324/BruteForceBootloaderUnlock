0d8a6ade5e1a248bf219b4ac60b5b14ba1c8d13d
Christian Groothoff <c.groothoff@oase.com>
2021-01-18 23:43:39 +0100

    Added new Solution with main project

---
+++ TryNBootLoader/TryNBootLoader.Programm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TryNBootLoader.Programm
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
